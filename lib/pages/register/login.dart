d0f1197b540c76bf009fb58090e91c0dd53ead0e
Grupo Pi <grupopiii27@gmail.com>
2025-09-27 20:07:41 -0300

    tela de login concluida

--- lib/pages/register/login.dart
+++ lib/pages/register/login.dart
@@ -18,7 +18,46 @@ class Login extends StatelessWidget {
             Image.asset(AppImage.logo, height: 150.0),
             SizedBox(height: 19.0),
             Align(child: Text('Bem Vindo(a) ao', style: Fonts.font24)),
-            Align(child: Text('ALUMA', style: Fonts.font24)),
+            Align(child: Text('ALUMA!', style: Fonts.font24)),
+            SizedBox(height: 111.0),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 49),
+              child: FloatingActionButton(
+                onPressed: null,
+                backgroundColor: Colors.white,
+                child: Text(
+                  'Entrar',
+                  style: TextStyle(
+                    color: Color(0xFF34629F),
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 21),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 49),
+
+              child: OutlinedButton(
+                onPressed: null,
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.white, width: 2),
+                  padding: EdgeInsets.all(15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(15),
+                  ),
+                ),
+                child: Text(
+                  'Criar Conta',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
